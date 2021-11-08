4f509a6d61709bf74ea7955871a84d9e131c8ae3
lovefxyo <809493461@qq.com>
2021-11-08 17:23:20 +0800

    将方法写成了头文件
    将方法写成了头文件

--- utils.h
+++ utils.h
@@ -1,6 +1,11 @@
 //
-// Created by Pygmalion on 2021/10/29.
+// Created by Pygmalion on 2021/11/8.
 //
+
+#ifndef DBUTILS_UTILS_H
+#define DBUTILS_UTILS_H
+
+
 #include<iostream>
 #include"mysql.h"
 #include<string>
@@ -8,24 +13,21 @@
 #include<vector>
 #include <iomanip>
 #include <map>
-
 using namespace std;
-
-class DBUtils{
+class utils {
 private:
-    const char *host="127.0.0.1";
-    const char *user="root";
-    const char *passwd="zdh622";
-    const char *dbname="usedbookstore";
-    const int port=3306;
+    const char *host;
+    const char *user;
+    const char *passwd;
+    const char *dbname;
+    int port;
 public:
     MYSQL mysql_conn;
-    MYSQL_RES *result;
+    MYSQL_RES *result=NULL;
     //查询的数据库名，目前是类中指定，可以后期更改
-    string QUERY_SCHEMA="usedbookstore";
-    /**
-     * 查询A表，对应B表
-     */
+    string QUERY_SCHEMA;
+    //用到的结构体
+    //外键结构体
     struct foreign_key{
         string btable_name;
         string bcolumn_name;
@@ -38,6 +40,15 @@ public:
         string column_name;
         string data_type;
     };
+
+    utils(const char *host, const char *user, const char *passwd, const char *dbname, char port){
+        this->host=host;
+        this->user=user;
+        this->passwd=passwd;
+        this->dbname=dbname;
+        this->port=port;
+        QUERY_SCHEMA=dbname;
+    }
     void init_mysql();
     void close_mysql();
     list<foreign_key> query_foreignkey(string TABLE_NAME);
@@ -47,7 +58,7 @@ public:
     map<string,vector<string>> query_coltabledata(string table_name);
 };
 
-void DBUtils::init_mysql() {
+void utils::init_mysql() {
     mysql_init(&mysql_conn);   //初始化MYSQL变量
     if (mysql_real_connect(&mysql_conn, host, user, passwd, dbname, 3306, 0, 0))  //连接到mysql 账号 密码 数据库名 端口
     {
@@ -58,13 +69,13 @@ void DBUtils::init_mysql() {
         cout << "----MySQL连接失败----" << endl;
     }
 }
-
-void DBUtils::close_mysql() {
+void utils::close_mysql() {
     mysql_close(&mysql_conn);
     mysql_free_result(result);
     result=NULL;
     cout << "----MySQL断开连接----" << endl;
 }
+
 /**
  * 查询表中外键的名字
  * 类给定了数据库名字(若不符合要求可以修改）
@@ -86,7 +97,7 @@ void DBUtils::close_mysql() {
     对应表中外键列名：sid
     -------------------------------------
  */
-list<DBUtils::foreign_key> DBUtils::query_foreignkey(string TABLE_NAME) {
+list<utils::foreign_key> utils::query_foreignkey(string table_name) {
     //建立连接
     init_mysql();
     MYSQL_FIELD *field = NULL;
@@ -95,22 +106,23 @@ list<DBUtils::foreign_key> DBUtils::query_foreignkey(string TABLE_NAME) {
             "select REFERENCED_TABLE_NAME,CONSTRAINT_NAME,REFERENCED_COLUMN_NAME,COLUMN_NAME from information_schema.key_column_usage t where t.constraint_schema='" +
             QUERY_SCHEMA +
             "' and t.constraint_name in (SELECT CONSTRAINT_NAME FROM information_schema.TABLE_CONSTRAINTS where CONSTRAINT_SCHEMA='" +
-            QUERY_SCHEMA + "' and CONSTRAINT_TYPE='FOREIGN KEY' and TABLE_NAME='" + TABLE_NAME + "')";
+            QUERY_SCHEMA + "' and CONSTRAINT_TYPE='FOREIGN KEY' and TABLE_NAME='" + table_name + "')";
     if (!mysql_query(&mysql_conn, sql.c_str()))  //若查询成功返回0，失败返回随机数
     {
         cout << "----查询外键成功----" << endl;
     }
     result = mysql_store_result(&mysql_conn);    //将查询到的结果集储存到result中
     list<foreign_key> foreign_list;
+    cout<<QUERY_SCHEMA;
     while ((row = mysql_fetch_row(result))) {
-        //do something with row[name_field]
         foreign_key foreignKey1;
         foreignKey1.bcolumn_name = row[2];
         foreignKey1.constraint_name = row[1];
         foreignKey1.btable_name = row[0];
         foreignKey1.acolumn_name=row[3];
-        foreignKey1.atable_name=TABLE_NAME;
+        foreignKey1.atable_name=table_name;
         foreign_list.push_back(foreignKey1);
+        cout<<"11";
     }
     close_mysql();
     return foreign_list;
@@ -133,8 +145,7 @@ list<DBUtils::foreign_key> DBUtils::query_foreignkey(string TABLE_NAME) {
     10:customeradr
     11:admininfo
  */
-
-list<string> DBUtils::query_tablename(string database_name) {
+list<string> utils::query_tablename(string database_name) {
     init_mysql();
     MYSQL_FIELD *field = NULL;
     MYSQL_ROW row;
@@ -169,7 +180,7 @@ list<string> DBUtils::query_tablename(string database_name) {
     表中字段为为：role--数据类型为:enum
     -------------------------------------
  */
-list<DBUtils::table_info> DBUtils::query_tableinfo(string table_name) {
+list<utils::table_info> utils::query_tableinfo(string table_name) {
     init_mysql();
     MYSQL_FIELD *field = NULL;
     MYSQL_ROW row;
@@ -203,7 +214,7 @@ list<DBUtils::table_info> DBUtils::query_tableinfo(string table_name) {
     20013 李红英 女 1999-01-01 不听指点，多绕弯弯。不懂装懂，永世饭桶。 100.000 13509230002
     20014 李新荣 男 1999-01-01 不问的人永远和愚昧在一起。 100.000 13509230003
  */
-vector<vector<string>> DBUtils::query_rowtabledata(string table_name) {
+vector<vector<string>> utils::query_rowtabledata(string table_name) {
     list<table_info> tableinfo_list=query_tableinfo(table_name);
     string columns;
     int num=tableinfo_list.size();
@@ -242,9 +253,9 @@ vector<vector<string>> DBUtils::query_rowtabledata(string table_name) {
     return tabledata_list;
 }
 /**
- *
+ *返回一个map<string,vector<string>>,key是字段属性名，vector是字段值
  */
-map<string, vector<string>> DBUtils::query_coltabledata(string table_name) {
+map<string, vector<string>> utils::query_coltabledata(string table_name) {
     list<table_info> tableinfo_list=query_tableinfo(table_name);
     int num=tableinfo_list.size();
     init_mysql();
@@ -276,63 +287,5 @@ map<string, vector<string>> DBUtils::query_coltabledata(string table_name) {
     close_mysql();
     return table_map;
 }
-int main(){
-    string table_name="accounts";
-    string database_name="usedbookstore";
-    DBUtils dbUtils;
-    /*
-    //测试查询外键
-    list<DBUtils::foreign_key> foreign_list=dbUtils.query_foreignkey(table_name);
-    cout<<"----外键共有："<<foreign_list.size()<<"个----"<<endl;
-    list<list<DBUtils::foreign_key>>::iterator it;
-    for(list<DBUtils::foreign_key>::iterator it=foreign_list.begin();it!=foreign_list.end();it++){
-        cout<<"-------------------------------------"<<endl;
-        cout<<"外键名为："<<it->constraint_name<<endl;
-        cout<<"查询表的表名:"<<it->atable_name<<endl;
-        cout<<"查询表中外键列名："<<it->acolumn_name<<endl;
-        cout<<"外键对应表名:"<<it->btable_name<<endl;
-        cout<<"对应表中外键列名："<<it->bcolumn_name<<endl;
-    }
-    cout<<"-------------------------------------"<<endl;
-    //测试查询数据库表名
-    list<string> schema_list=dbUtils.query_tablename(database_name);
-    list<string>::iterator it1;
-    cout<<"----"<<database_name<<"共有"<<schema_list.size()<<"个表----"<<endl;
-    int i=1;
-    for(it1=schema_list.begin();it1!=schema_list.end();it1++,i++){
-        cout<<i<<":";
-        cout<<it1->c_str()<<endl;
 
-    }
-    //测试查询表信息
-    list<DBUtils::table_info> tableinfo_list =dbUtils.query_tableinfo("accounts");
-    list<string>::iterator it2;
-    cout<<"----"<<dbUtils.QUERY_SCHEMA<<"共有"<<tableinfo_list.size()<<"个字段----"<<endl;
-    for(list<DBUtils::table_info>::iterator it2=tableinfo_list.begin();it2!=tableinfo_list.end();it2++){
-        cout<<"-------------------------------------"<<endl;
-        cout<<"表中字段为为："<<it2->column_name<<"--";
-        cout<<"数据类型为:"<<it2->data_type<<endl;
-    }
-    cout<<"-------------------------------------"<<endl;
-*/
-    //测试查询数据
-    vector<vector<string>> tabledata_list = dbUtils.query_rowtabledata(table_name);
-    cout<<setiosflags(ios::left);
-    for(int i=0;i<tabledata_list.size();i++){
-        for(int j=0;j<tabledata_list[i].size();j++){
-            cout<<tabledata_list[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-
-    map<string,vector<string>> table_map;
-    table_map=dbUtils.query_coltabledata(table_name);
-    cout<<"success"<<table_map.size();
-    map<string,vector<string>>::iterator it;
-    for(it=table_map.begin();it!=table_map.end();it++){
-        vector<string> v =it->second;
-        for(int i=0;i<v.size();i++){
-            cout<<it->first<<":"<<v[i]<<endl;
-        }
-    }
-}
+#endif //DBUTILS_UTILS_H
